e78134650f579f0cb4afaeef568192cf4b333636
null null <gbisgenius@gmail.com>
2020-05-24 23:47:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+fname=str(input("Enter first name: "))
+lname=str(input("Enter last name: "))
+address=str(input("Enter your address :"))
+zip=int(input("Enter Zip code: "))
+yob=int(input ("Enter your Year of Birth :"))
+
+age=2020-yob
+print (f''' My First name: {fname}
+My last name : {lname}
+My Address: {address}
+My zip: {zip}
+My age : {age}
+''')
